e4827e8069a64ac746de31b1f98e35d48deb9800
Magdalena Kołtuniak <m.koltuniak@gmail.com>
2026-04-08 16:29:50 +0200

    add task 2

--- 02/app.js
+++ 02/app.js
@@ -1,28 +1,40 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    const clickEl = document.querySelector('.error--click');
-    const enterEl = document.querySelector('.error--enter');
+	const clickEl = document.querySelector('.error--click');
+	const enterEl = document.querySelector('.error--enter');
+	const alert = document.querySelector('.alert');
 
-    setRandomPosition(clickEl);
-    setRandomPosition(enterEl);
+	alert.addEventListener('click', () => {
+		alert.classList.add('alert--hidden');
+	});
 
-    initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
-    initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
+	setRandomPosition(clickEl);
+	setRandomPosition(enterEl);
 
+	initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
+	initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
 }
 
 function setRandomPosition(element, error = null) {
-    element.style.top = Math.random() * 600 + 'px';
-    element.style.left = Math.random() * 800 + 'px';
+	try {
+		element.style.top = Math.random() * 600 + 'px';
+		element.style.left = Math.random() * 800 + 'px';
 
-    if(error) {
-        throw error;
-    }
+		if (error) {
+			throw error;
+		}
+	} catch (e) {
+        const alert = document.querySelector('.alert');
+		const alertMessage = alert.querySelector('.alert__message');
+
+		alertMessage.textContent = e.message;
+		alert.classList.remove('alert--hidden');
+	}
 }
 
 function initEventWithError(element, eventName, error) {
-    element.addEventListener(eventName, function() {
-        setRandomPosition(this, error);
-    })
+	element.addEventListener(eventName, function () {
+		setRandomPosition(this, error);
+	});
 }
