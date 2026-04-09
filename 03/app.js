19120d449a6b91a86d6de1e77cc778608f1de949
Magdalena Kołtuniak <m.koltuniak@gmail.com>
2026-04-09 13:03:42 +0200

    add implementation for min > max validation

--- 03/app.js
+++ 03/app.js
@@ -2,5 +2,10 @@ export default function randomNumber(min, max) {
 	if (typeof min !== 'number' || typeof max !== 'number') {
 		throw new Error('Arguments must be numbers');
 	}
+
+	if (min > max) {
+		throw new Error('Min cannot be greater than max');
+	}
+    
 	return 1;
 }
