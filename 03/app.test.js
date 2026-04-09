60828d9d3c2fbb80e35a671979632c3b07e2ba1e
Magdalena Kołtuniak <m.koltuniak@gmail.com>
2026-04-09 12:17:45 +0200

    add test for non-number min argument

--- 03/app.test.js
+++ 03/app.test.js
@@ -4,4 +4,8 @@ describe('randomNumber', () => {
 	it('returns 1 if min and max are both equal to 1', () => {
 		expect(randomNumber(1, 1)).toBe(1);
 	});
+
+	it('throws an error if min is not a number', () => {
+		expect(() => randomNumber('x', 3)).toThrow();
+	});
 });
